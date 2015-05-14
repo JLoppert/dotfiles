fd08076eb29a4af376659accd6a4e459befe2c72
Jeffrey Loppert <Jeffrey.Loppert@mathworks.com>
2015-05-14 17:04:31 -0400

    Fixed bug to use absolute path when setting symbolic links

--- linux/symlinks.sh
+++ linux/symlinks.sh
@@ -1,12 +1,12 @@
-LINUX=`dirname $BASH_SOURCE`
+LINUX=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT=`dirname $LINUX`
 
 # Bash
-ln -s $ROOT/bash/.bash_profile ~/.bash_profile
+ln -sf $ROOT/bash/.bash_profile ~/.bash_profile
 
 # Sublime Text 3
-ln -s $ROOT/sublime/User ~/.config/sublime-text-3/Packages/
+ln -sf $ROOT/sublime/User ~/.config/sublime-text-3/Packages/
 
 # Git
-ln -s $ROOT/git/git_branch_terminal.sh ~/.git_branch_terminal.sh
-ln -s $ROOT/git/git_completion_terminal.sh ~/.git_completion_terminal.sh
+ln -sf $ROOT/git/git_branch_terminal.sh ~/.git_branch_terminal.sh
+ln -sf $ROOT/git/git_completion_terminal.sh ~/.git_completion_terminal.sh
