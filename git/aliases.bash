e359b91d0a949bfc28dc450ea81d7b88ea792d76
Jeffrey Loppert <Jeffrey.Loppert@mathworks.com>
2015-05-15 11:39:43 -0400

    Updated git

--- git/aliases.bash
+++ git/aliases.bash
@@ -2,13 +2,18 @@
 alias cdg='cd $(git rev-parse --show-toplevel || pwd)'
 alias get='git'
 
+alias gau='git add -u'
+alias gaa='git add -A'
+alias gr='git reset HEAD'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gcm='git commit -m'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
-alias gb='git branch'
+alias gb='git branch --color'
+alias gbc='git rev-parse --abbrev-ref HEAD' # current branch
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
--- git/git_branch_terminal.bash
+++ git/git_branch_terminal.bash
@@ -1,11 +1,11 @@
 # show the git branch in the command line
 function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+  git rev-parse --abbrev-ref HEAD
 }
 
 function proml {
 # user@host:dir(branch)$
-PS1="\\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\[\e[m\]\$ "
+PS1="\\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\](\$(parse_git_branch))\[\e[m\]\$ "
 PS2='> '
 PS4='+ '
 }
