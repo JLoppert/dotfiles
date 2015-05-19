346dd4e36cb8e14058ca87353c9d710cf9dd83fd
Jeffrey Loppert <Jeffrey.Loppert@mathworks.com>
2015-05-19 09:07:49 -0400

    Updated aliases. Added quick git manual lookup

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,19 +1,35 @@
 # Change to the root level directory the current git repository
 alias cdg='cd $(git rev-parse --show-toplevel || pwd)'
-alias get='git'
 
+# Man
+g () {
+  man "git-$1"
+}
+
+# Status
+alias s='git status'
+alias gs='git status -sb'
+
+# Add
+alias ga='git add -A'
 alias gau='git add -u'
-alias gaa='git add -A'
-alias gr='git reset HEAD'
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
+
+# Commit
 alias gc='git commit'
-alias gcm='git commit -m'
-alias gca='git commit -a'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
+# Undo
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
+alias gr='git reset HEAD'
+
+# Push/Pull
+alias gp='git pull --prune'
+alias gpo='git push origin HEAD'
+
+# Branch
 alias gb='git branch --color'
+alias gbn='git checkout -b' # create new branch
 alias gbc='git rev-parse --abbrev-ref HEAD' # current branch
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+# Diff
+alias gd='git diff'
