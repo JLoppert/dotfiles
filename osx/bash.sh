f612fd868ec44d756a59eae7f050aebe13e31f7b
Jeffrey Loppert <Jeffrey.Loppert@outlook.com>
2015-05-19 21:25:54 -0400

    Added neccessary relative paths

--- osx/bash.sh
+++ osx/bash.sh
@@ -1,6 +1,8 @@
 # setup .bash_profile
 # append absolute paths to *.bash files
 # ex: bash/alias.bash
+OSX=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT=`dirname $OSX`
 
 # git should ignore changes to bash_profile
 git update-index --assume-unchanged $ROOT/bash/.bash_profile
