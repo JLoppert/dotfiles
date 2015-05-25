0da62623ebcc695641c5372a5216101cb5f99f1c
Jeffrey Loppert <Jeffrey.Loppert@outlook.com>
2015-05-24 23:56:51 -0400

    Fixed issue where completion file was not found

--- git/git_completion_custom.bash
+++ git/git_completion_custom.bash
@@ -1,13 +1,8 @@
-# replace everything up to last / with ''
-# TODO: include directory
-# path/to/file/name.extension => name.extension
-# path/to/directory/ =>
 _g_modified()
 {
     local cur=${COMP_WORDS[COMP_CWORD]}
-    modified_files=`ruby git_completion_filenames.rb`
-    # echo "$modified_files"
-    # modified_directories=`$modified | sed -e 's/.*\///g'`
+    path="$( dirname "${BASH_SOURCE[0]}" )"
+    modified_files=`ruby $path/git_completion_filenames.rb`
     COMPREPLY=( $(compgen -W "$modified_files" -- $cur) )
 }
 
