955b926f95a81e6a67045e1409c2416dbf99bbe8
Jeffrey Loppert <Jeffrey.Loppert@mathworks.com>
2015-05-26 14:41:44 -0400

    fullpath.rb returns . if provided file is not found

--- git/git_completion_fullpath.rb
+++ git/git_completion_fullpath.rb
@@ -1,11 +1,13 @@
 file = ARGV[0]
 files = `git status --porcelain`
 
+# diff everything if file not found
+full_path = '.'
+
 files.each_line do |f|
-  full_path = f.split(' ').last
   file_name = f.split('/').last
   file_name = f.split('/')[-2] if file_name.length == 1
-  full_path if file_name.strip == file
+  full_path = f.split(' ').last if file_name.strip == file.strip
 end
 
 puts full_path
